e291b174ac5b7a87d3a343846dadfa2a85d5eeb3
Xavier Cao <xavier.cao.goldenowl@gmail.com>
2023-08-15 15:22:17 +0700

    using generic class based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,38 +1,13 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetListView(mixins.ListModelMixin,
-	mixins.CreateModelMixin,
-	generics.GenericAPIView):
+class SnippetListView(generics.ListCreateAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer		
 
+
+class SnippetDetailView(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-		
-
-
-class SnippetDetailView(mixins.RetrieveModelMixin,
-	mixins.UpdateModelMixin,
-	mixins.DestroyModelMixin,
-	generics.GenericAPIView):
-
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
-
-
